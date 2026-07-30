0d791400f40083ae86eb5a337e767e06d960442d
Joshian Ruiz <joshianruiz55@gmail.com>
2026-07-29 21:21:34 -0600

    prueba

--- supabase-config.js
+++ supabase-config.js
@@ -2,6 +2,6 @@
  * Datos públicos del proyecto Supabase.
  */
 window.SUPABASE_CONFIG = {
-  url: 'https://whyzqrrjmptmaxqmacvm.supabase.co/rest/v1/',
+  url: 'https://whyzqrrjmptmaxqmacvm.supabase.co',
   publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoeXpxcnJqbXB0bWF4cW1hY3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwOTg3NDgsImV4cCI6MjEwMDY3NDc0OH0.vmAMcjLsWSELVDwRNnnBw7KZKob17ZQ2tRQpIhjqOn8'
 };
