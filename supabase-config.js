df24b6f7ee7abb516db8a02649c60cbfc45cdc90
Joshian Ruiz <joshianruiz55@gmail.com>
2026-07-29 21:20:15 -0600

    prueba

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,5 @@
  */
 window.SUPABASE_CONFIG = {
   url: 'https://whyzqrrjmptmaxqmacvm.supabase.co/rest/v1/',
-  publishableKey: 'sb_publishable_S05Dd2Gzj0JzazUx0-wrtA_iQmNsf30'
+  publishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoeXpxcnJqbXB0bWF4cW1hY3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwOTg3NDgsImV4cCI6MjEwMDY3NDc0OH0.vmAMcjLsWSELVDwRNnnBw7KZKob17ZQ2tRQpIhjqOn8'
 };
